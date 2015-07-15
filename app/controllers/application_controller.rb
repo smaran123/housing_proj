0f6497abb9fc00ce39ad53428ac1b5fc13bccb4f
bhagathreddy123 <katta_reddy04@yahoo.com>
2015-07-15 22:57:40 +0530

    admin signup redirect to admin signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :admin_login
   layout :admin_layout
   def after_sign_in_path_for(resource_or_scope)
    if current_admin
@@ -23,4 +24,17 @@ def after_sign_up_path_for(resource_or_scope)
   after_sign_in_path_for(resource_or_scope)
 end
 
+def admin_login
+   if !current_admin && params[:controller] == "devise/registrations"
+   
+   redirect_to new_admin_session_path
+   end
+   end
+   
 end
+
+
+  
+
+
+  
